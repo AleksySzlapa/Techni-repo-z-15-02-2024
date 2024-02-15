1bd7eb4565710d42f2c0e9f6a0d8355b5cf11f24
Aleksy Szłapa <u8_aleszl_lbn@technischools.com>
2024-02-15 10:26:03 +0100

    dodanie testów

--- test_main.py
+++ test_main.py
@@ -1,22 +1,35 @@
 from unittest import TestCase
 import unittest
-
+from main import file_sumer
 class Test(unittest.TestCase):
+
     def test_non_existing_file(self):
-        self.fail()
+        file = 'thios path doesnt exist'
+        with self.assertRaises(FileNotFoundError):
+            result = file_sumer(file)
+
 
     def test_empty_file(self):
-        self.fail()
+        file = 'test2.txt'
+        expected = 0
+        result = file_sumer(file)
+        self.assertEqual(expected, result)
 
-    def test_all_positives(self):
-        self.fail()
+    def test_all_in_range(self):
+        file = 'test3.txt'
+        expected = 6
+        result = file_sumer(file)
+        self.assertEqual(expected, result)
 
-    def test_all_negatives(self):
-        self.fail()
+    def test_out_of_range(self):
+        file = 'test4.txt'
+        with self.assertRaises(ValueError):
+            result = file_sumer(file)
 
-    def test_mix(self):
-        self.fail()
-
+    def test_only_plain_text(self):
+        file = 'test5.txt'
+        with self.assertRaises(TypeError):
+            result = file_sumer(file)
 
 if __name__ == '__main__':
     unittest.main()
